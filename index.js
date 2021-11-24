f0fd8ae31424fa58e510dbc38aec85fff1991ac9
Sharon Apot <sharonapot@gmail.com>
2021-11-24 08:21:18 +0300

    Add index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+//Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
+
+ //where;
+
+    //CC - is the century digits. For example 1989 has CC = 19
+    //YY - is the Year digits (1989 has YY = 89)
+    //MM - is the Month
+    //DD - is the Day of the month 
+    //mod - is the modulus function ( % )
+
+    var d=((CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD % 7;
+    return d;
