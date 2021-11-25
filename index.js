dbdb0014030b5d73fc9dd4404f7520973dd1ce62
Sharon Apot <sharonapot@gmail.com>
2021-11-25 09:36:02 +0300

    add java script code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
-
- //where;
+ 
+//where;
 
     //CC - is the century digits. For example 1989 has CC = 19
     //YY - is the Year digits (1989 has YY = 89)
@@ -8,5 +8,27 @@
     //DD - is the Day of the month 
     //mod - is the modulus function ( % )
 
-    var d=((CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD % 7;
-    return d;
+function getInputValue(){
+    var dayValue = document.getElementsByName("day").value;
+    var monthValue = document.getElementsByName ("month").value;
+    var yearvalue = document.getElementsByName ("year").value;
+}
+
+function getRadioCheckedValue(){
+  
+    }
+    
+//Day of the week (d) = ( ( (cc/4) -2*CC-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) mod 7
+
+var dayOfWeek = d;
+d = ( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7;
+
+var yy= year
+var cc = console.log(year.slice(0,1));
+var mm = month
+var dd = day
+
+
+
+
+ 
