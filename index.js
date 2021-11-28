abe8ddf14d5c74c1596487fd39293e067131df02
Sharon Apot <sharonapot@gmail.com>
2021-11-28 12:10:18 +0300

    Change heading

--- index.js
+++ index.js
@@ -8,3 +8,11 @@ window.onload = function () {
         year.appendChild(option);
     }
 };
+
+//Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
+//where;
+//CC - is the century digits. For example 1989 has CC = 19
+//YY - is the Year digits (1989 has YY = 89)
+//MM -  is the Month
+//DD - is the Day of the month 
+//mod - is the modulus function ( % )
