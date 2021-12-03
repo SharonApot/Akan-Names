a64f45b7d29f67cddf0d01cd2da5bbf94c183e1e
Sharon Apot <sharonapot@gmail.com>
2021-12-03 21:55:52 +0300

    Add java codes for the input elements

--- index.js
+++ index.js
@@ -10,14 +10,21 @@ window.onload = function () {
   }
 };
 
-function myFunction() {
+function myFunction() {   
   // Get the value of the input fields
   let x = document.getElementById("day").value;
   let y = document.getElementById("month").value;
   var select = document.getElementById("year");
   var z = select.options[select.selectedIndex].value;
   console.log(z);
-  
+
+  var genderInput = document.getElementsByName("gender");
+  var gen = "";
+  for (var i = 0; i < genderInput.length; i++) {
+    if (genderInput[i].checked) {
+      gen = (genderInput[i].value);
+    }
+}
   // If x is Not a Number or less than one or greater than 31
   // If y is Not a Number or less than one or greater than 12
   
@@ -25,7 +32,7 @@ function myFunction() {
   if ((isNaN(x) || x < 1 || x > 31) || (isNaN(y) || y < 1 || y> 12)){
     text = "Input not valid. Enter correct day and Month";
   } else {
-    text = "Your Birthdate is " + (x)+"/"+ (y) + "/" +(z);
+    text = " You are a  " + (gen) + " And Your Birthdate is " + (x)+"/"+ (y) + "/" +(z);
   }
   document.getElementById("print").innerHTML = text;
 }
